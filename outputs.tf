97a53c8607afc14a9fb3126206bdba37146bdef0
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:18:43 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_machine_data_disk_attachments" {
+  description = "All virtual_machine_data_disk_attachment resources"
+  value       = azurerm_virtual_machine_data_disk_attachment.virtual_machine_data_disk_attachments
+}
+
